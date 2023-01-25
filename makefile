20259e922c4c2e40f51e128a85a891cd3a3925dc
commandblockguy <bluestemjc@gmail.com>
2023-01-25 13:31:24 -0600

    oh look I'm an app now

--- makefile
+++ makefile
@@ -3,14 +3,15 @@
 # ----------------------------
 
 NAME = UNITS
-ICON = icon.png
-DESCRIPTION = "Unit Manipulation Tool"
-COMPRESSED = NO
-ARCHIVED = YES
+DESCRIPTION = Unit Manipulation Tool
 
 CFLAGS = -Wall -Wextra -Oz
 CXXFLAGS = -Wall -Wextra -Oz
 
+APP_NAME = Units
+APP_VERSION = 5.0.0.0000
+APP_INST_NAME = UNITINST
+
 # ----------------------------
 
-include $(shell cedev-config --makefile)
+include app_tools/makefile
--- src/main.c
+++ src/main.c
@@ -7,15 +7,14 @@
  *--------------------------------------
 */
 
+#undef NDEBUG
+
 #include <capnhook.h>
 #include <fileioc.h>
 #include <debug.h>
 
 extern void parse_hook;
 extern void hs_hook;
-
-#define RAM_START ((void*)0xD00000)
-#define PRGM_ADDR ((void*)0xD1A87F)
 
 int main(int argc, const char **argv)
 {
@@ -32,28 +31,11 @@ int main(int argc, const char **argv)
 
     if(!argc) return 1;
 
-    // It's time to reflect upon one of the great mysteries of life
-    // How does one find themselves?
-    ti_var_t slot = ti_OpenVar(argv[0], "r", TI_PRGM_TYPE);
-    if(!slot) return 1;
-    void *self = ti_GetDataPtr(slot);
-    if(!self) return 1;
-    ti_Close(slot);
-    // It's that simple.
-
-    if(self >= RAM_START) {
-        // Sometimes you find yourself in RAM.
-        // RAM is a terrible place to be, let's not be there.
-        return 1;
-    }
-
-    dbg_printf("Program at %p, running at %p, hooks at %p/%p and %p/%p\n", self, PRGM_ADDR, &parse_hook - PRGM_ADDR + self, &parse_hook, &hs_hook - PRGM_ADDR + self, &hs_hook);
-
     hook_error_t err;
-    err = hook_Install(0x000900, &parse_hook - PRGM_ADDR + self, 0, HOOK_TYPE_PARSER, 5, "Unit manipulation hook");
-    dbg_printf("Err %u installing hook at %p\n", err, &parse_hook - PRGM_ADDR + self);
-    err = hook_Install(0x000901, &hs_hook - PRGM_ADDR + self, 0, HOOK_TYPE_HOMESCREEN, 5, "Unit manipulation hook");
-    dbg_printf("Err %u installing hook at %p\n", err, &hs_hook - PRGM_ADDR + self);
+    err = hook_Install(0x000900, &parse_hook, 0, HOOK_TYPE_PARSER, 5, "Unit manipulation hook");
+    dbg_printf("Err %u installing hook at %p\n", err, &parse_hook);
+    err = hook_Install(0x000901, &hs_hook, 0, HOOK_TYPE_HOMESCREEN, 5, "Unit manipulation hook");
+    dbg_printf("Err %u installing hook at %p\n", err, &hs_hook);
     hook_Sync();
 
     // Enable lowercase
